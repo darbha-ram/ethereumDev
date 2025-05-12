a712e40cedc31c07bc9fa10c9b645b0dc20fd99c
Ram Darbha <darbha.ram@gmail.com>
2025-05-12 23:15:40 +0000

    Update hardhat contract address vars

--- scripts/sethardhatvars.sh
+++ scripts/sethardhatvars.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-# for Hardhat local network
-npx hardhat vars set MITCOIN_CONTRACT_ADDR "0x68B1D87F95878fE05B998F19b66F4baba5De1aed"
-npx hardhat vars set MYSABLIERFLOW_CONTRACT_ADDR "0xc6e7DF5E7b4f2A278906862b61205850344D4e7d"
-npx hardhat vars set FLOWCREATOR_CONTRACT_ADDR "0x59b670e9fA9D0A427751Af201D676719a970857b"
+# for Hardhat local network - only valid till 'npx hardhat node' is terminated
+npx hardhat vars set MITCOIN_CONTRACT_ADDR "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+npx hardhat vars set MYSABLIERFLOW_CONTRACT_ADDR "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+npx hardhat vars set FLOWCREATOR_CONTRACT_ADDR "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9"
 
 
